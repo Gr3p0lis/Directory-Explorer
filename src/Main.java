c7db40f7cc60fc795a7bd649a46f7210798670c8
Matteo Riccardo Manca <63729798+Gr3p0lis@users.noreply.github.com>
2024-05-24 09:15:38 +0900

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        Path startingPath = Path.of("."); // current directory
+        FileVisitor<Path> statsVisitors = new StatsVisitor();
+
+        try {
+            Files.walkFileTree(startingPath, statsVisitors);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static class StatsVisitor extends SimpleFileVisitor<Path> {
+
+        private int level;
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                throws IOException {
+            Objects.requireNonNull(file);
+            Objects.requireNonNull(attrs);
+
+            System.out.println("\t".repeat(level + 1) + file.getFileName());
+
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            Objects.requireNonNull(dir);
+            Objects.requireNonNull(attrs);
+            level++;
+            System.out.println("\t".repeat(level) + dir.getFileName());
+            return FileVisitResult.CONTINUE;
+        }
+
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Objects.requireNonNull(dir);
+//            if (exc != null)
+//                throw exc;
+            level--;
+            return FileVisitResult.CONTINUE;
+        }
+
+    }
+
+
+}
